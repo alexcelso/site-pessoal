ca4886877d803399b25e33f24c7be2ec23852118
Alex <alexmcmc@hotmail.com>
2017-07-27 02:24:48 -0300

    modificando de  html para php

---
+++ includes/pages/contato.php
@@ -0,0 +1,42 @@
+<section class="contato">
+					<div class="container">
+						<form>
+						 <div class="cols">
+						  <div class="col">
+							<div class="form-group">
+								<input type="text" placeholder="Nome" required="">
+							</div>
+						  </div>
+						  
+						  <div class="col">	
+							<div class="form-group">
+								<input type="email" placeholder="E-mail" required="">
+							</div>
+						   </div>
+						   <div class="col">	
+							<div class="form-group">
+								<input type="number" placeholder="Telefone" required="">
+							</div>
+						   </div>
+						   <div class="col">	
+							<div class="form-group">
+								<select name="" id="">
+									<option>Selecione uma opção</option>
+									<option>Orçamento</option>
+									<option>Sugestão</option>
+									<option>Outras Informações</option>
+								</select>
+							</div>
+						    </div>
+						    </div>	
+							<div class="form-group">
+								<textarea rows="10" placeholder="Mensagem"></textarea>
+							</div>
+							<div class="form-group">
+								<button class="botao medio"><i class="fa fa-arrow-right"></i>Enviar</button>
+							</div>
+							<p class="bg-success"><i class="fa fa-info-circle"></i>  Seu contato foi enviado com sucesso!</p>
+                            <p class="bg-danger"><i class="fa fa-info-circle"></i> Preencha todos os campos obrigatórios.</p>
+						</form>
+					</div>
+				</section>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+  $style = 'home';
+  $chamada = '<strong>Alex</strong> M. <strong>Celso</strong>';
+  include_once('header.php');
+  include_once('includes/pages/home.php');
+  include_once('footer.php');
+?>
